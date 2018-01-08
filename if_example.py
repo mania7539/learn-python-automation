4d35058265b1e0e000a1ae5111f7a06b4c511601
David.Rz.Liu <mania7539@gmail.com>
2018-01-08 16:29:38 +0800

    Practice: if, elif

--- if_example.py
+++ if_example.py
@@ -11,3 +11,16 @@ if password == "swordfish":
     print("Access granted.");
 else:
     print("Wront password.");
+
+
+name = "Bob";
+age = 3000;
+if name == "Alice":
+    print("Hi Alice");
+elif age < 12:
+    print("You are not Alice, kiddo.");
+elif age > 2000:
+    print("Unlike you, Alice is not an undead, immortal vampire.");
+elif age > 1000:
+    print("You are not Alice, grannie.");
+
