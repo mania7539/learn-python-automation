aba14c83adcd894b156c85b2bbff99920cd6a5ba
David.Rz.Liu <mania7539@gmail.com>
2018-01-08 16:22:19 +0800

    Practice: if else with blocks : and indentation

---
+++ if_example.py
@@ -0,0 +1,13 @@
+name = "Alice";
+if name == "Alice":
+    print("Hi Alice");
+print("Done");
+
+
+print("Please enter your password: ");
+password = input();
+
+if password == "swordfish":
+    print("Access granted.");
+else:
+    print("Wront password.");
