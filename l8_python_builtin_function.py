e8b466229dca915cfe546714abe7e08f9c042065
David.Rz.Liu <mania7539@gmail.com>
2018-01-10 00:53:16 +0800

    Intro: import modules - random, sys

---
+++ l8_python_builtin_function.py
@@ -0,0 +1,44 @@
+
+# Import modules before you can use them
+#
+# Generate random number between 1 and 10
+#   Use "random." at front to tell python to look inside that module for the function
+#   since "randint()" is not a builtin function, it only exist inside the random module
+"""
+import random;
+
+num = random.randint(1, 10);
+print(num);
+
+"""
+"""output: 2, 10, 8, ..."""
+
+
+# We can import multiple modules in a single line by separate module names with ","
+"""
+import random, sys, os, math;
+"""
+
+
+# Since we import everything (*) in random, so it now performs like alias.
+#   We can ignore the "random." for later coding
+"""
+from random import *
+num = randint(1, 10);
+print(num);
+"""
+"""output: 2, 10, 8, ..."""
+
+
+# Use sys.exit() to terminate a python program early
+"""
+import sys;
+print("Hello");
+sys.exit();
+print("Goodbye");
+"""
+"""output: Hello (Console exits so "Goodbye" not printed)"""
+
+
+# 
+
