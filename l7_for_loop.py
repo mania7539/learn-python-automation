b30f9a07bb80333cae9112612a0af15830709c33
David.Rz.Liu <mania7539@gmail.com>
2018-01-10 00:36:09 +0800

    Try: for loop should work with range function

---
+++ l7_for_loop.py
@@ -0,0 +1,58 @@
+
+# for loop
+"""
+print("My name is: ");
+for i in range(5):
+    print("Jimmy 5 Times " + str(i));
+"""
+"""output: (Jimmy 5 Times) 0 1 2 3 4"""
+
+
+# for loop to sum up 0 - 100 [0, 100)
+"""
+total = 0;
+for i in range (101):
+    total += i;
+print(total);
+"""
+"""output: 5050 , it takes less than 1 sec to complete""" 
+
+
+# for loop starts from "NONE 0", [12, 17)
+"""
+print("My name is: ");
+for i in range(12, 17):
+    print("Jimmy 5 Times " + str(i));
+"""
+"""output: (Jimmy 5 Times) 12 13 14 15 16"""
+
+
+# for loop starts from "NONE 0" and increase "NONE 1" for each iteration
+# [1, 11)
+"""
+print("My name is: ");
+for i in range(1, 11, 2):
+    print("Jimmy 5 Times " + str(i));
+"""
+"""output: (Jimmy 5 Times) 1 3 5 7 9"""
+
+
+# for loop decrease 1 for each iteration [5, -1)
+"""
+print("My name is: ");
+for i in range(5, -1, -1):
+    print("Jimmy 5 Times " + str(i));
+"""
+"""output: (Jimmy 5 Times) 5 4 3 2 1 0"""
+
+
+
+# for loop decrease 1 for each iteration [5, -1)
+
+print("My name is: ");
+for i in range(5, -1, -1):
+    print("Jimmy 5 Times " + str(i));
+
+"""output: (Jimmy 5 Times) 5 4 3 2 1 0"""
+
+
